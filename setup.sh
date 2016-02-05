25b2fb10060f96f0fed4baced45bb627e1f1039a
Alex Kaplan <kaplan2539@gmail.com>
2016-02-05 12:22:25 -0800

    added libusb and cmake 3.4 to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get -y install g++ build-essential cmake libgtk2.0-dev
+sudo apt-get -y install g++ build-essential cmake libgtk2.0-dev libusb-1.0-0-dev
 
 cmake --version
-
+wget https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz
+tar xvzf cmake-3.4.3.tar.gz
+cd cmake-3.4.3
+./bootstrap && make && sudo make install
+cd ..
